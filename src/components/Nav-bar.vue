bbd432e4eb67cbe4abe6abb9bf4b2c5612369ea6
RafaelZucarelli <zucarelli83@gmail.com>
2024-01-25 14:42:49 -0300

    atualizado

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -1,16 +1,14 @@
 <template>
   <v-app>
-    <!-- Menu lateral -->
+  
     <v-navigation-drawer app dark color="blue" v-model="drawer">
       <v-list>
         <v-list-item to="/HomePage" class="nav-link">Página Inicial</v-list-item>
         <v-list-item to="/Notes" class="nav-link">Anotações</v-list-item>
-        <v-list-item to="/about" class="nav-link">Sobre</v-list-item>
-        <!-- Adicione outros links conforme necessário -->
+      
       </v-list>
     </v-navigation-drawer>
-
-    <!-- Botão para abrir o menu lateral -->
+    
     <v-app-bar app dark color="blue">
       <v-btn icon @click="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
@@ -18,10 +16,9 @@
       <v-toolbar-title class="logo">Bloco de Anotações</v-toolbar-title>
     </v-app-bar>
 
-    <!-- Conteúdo Principal -->
     <v-main>
       <v-container>
-        <router-view></router-view> <!-- Apenas uma instância de router-view aqui -->
+        <router-view></router-view> 
       </v-container>
     </v-main>
   </v-app>
@@ -43,28 +40,22 @@ export default {
   color: white;
   font-size: 1.5em;
 }
-
 .nav-link {
-  color: black; /* Ajuste a cor conforme necessário */
+  color: black; 
   text-decoration: none;
   padding: 8px 12px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
 }
-
 .nav-link:hover {
   background-color: #555;
   text-decoration: underline;
 }
-
-/* Estilizar o link ativo */
 .nav-link.router-link-active {
   background-color: #555;
   font-weight: bold;
 }
-
-/* Adicione padding à esquerda para afastar o conteúdo da barra lateral */
 .v-main {
-  padding-left: 300px; /* Ajuste conforme necessário */
+  padding-left: 300px; 
 }
 </style>
